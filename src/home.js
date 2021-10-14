1b2bdb196600e8fd533b459d9441fc2b0a355879
SurajiEkanayaka <surajiekanayaka4@gmail.com>
2021-10-14 11:11:31 +0530

    updated

--- src/home.js
+++ src/home.js
@@ -156,6 +156,65 @@ function Home(){
         </MDBCard>
       </MDBCol>
     </MDBRow>
+    <br/>
+    <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src='https://mdbcdn.b-cdn.net/img/new/standard/city/044.jpg'
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This is a longer card with supporting text below as a natural lead-in to additional content.
+              This content is a little bit longer.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src='https://mdbcdn.b-cdn.net/img/new/standard/city/043.jpg'
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This card has supporting text below as a natural lead-in to additional content.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src='https://mdbcdn.b-cdn.net/img/new/standard/city/042.jpg'
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This
+              card has even longer content than the first to show that equal height action.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small className='text-muted'>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
 
     </MDBContainer>
         </>
